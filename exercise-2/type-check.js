932494172552dc8bb44ad2d9e19edac77a318b71
mlima <milan.lima@hotmail.fr>
2020-10-23 17:16:03 +0200

    update exercise-2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -14,3 +14,25 @@ function type_check_v1(value,type){
         }
     }    
 }
+
+function type_check_v2(value,conf){
+    for(key in conf){
+        switch(key){
+            case 'type':
+                if(!type_check_v1(value,conf.type)) return false;
+                break;
+            case "value":
+                if(JSON.stringify(value) !== JSON.stringify(conf.value)) return false;
+                break;
+            case "enum":
+                let found = false;
+                for (subvalue of conf.enum){
+                    found = type_check_v2(value, {value: subvalue});
+                    if(found) break;
+                }
+                if (!found) return false;
+                break;
+        }
+    }
+    return true;
+}
