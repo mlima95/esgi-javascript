156c0de2c8bfa34b39b2f67e40361a18ddfb1a5b
mlima <milan.lima@hotmail.fr>
2020-10-23 15:49:05 +0200

    update exercise-2

--- exercise-2/type-check.js
+++ exercise-2/type-check.js
@@ -1,24 +1,16 @@
 function type_check_v1(value,type){
     if(typeof(value) !== "object"){
-        switch(typeof(value)){
-            case "function":
-            case "undefined":
-            case "boolean":
-            case "string":
-            case "number":
-            case "symbol":
-                return type === typeof(value); 
-        }
+        return type === typeof(value); 
     }else{
         switch(type){
             case "array":
-                console.log('is array');
                 return Array.isArray(value);
             case "null":
                 return value === null;
-            default:
+            case "object":
                 return !Array.isArray(value) && value !== null;
+            default:
+                return false;
         }
-    }
-        
+    }    
 }
