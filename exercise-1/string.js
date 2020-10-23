9a2fbf70267e206f592881fd238a65b2758410f6
mlima <milan.lima@hotmail.fr>
2020-10-23 14:42:55 +0200

    update exercise-1

--- exercise-1/string.js
+++ exercise-1/string.js
@@ -53,3 +53,30 @@ function yoda(string){
     
 }
 
+function vig(string, code) {
+    if (typeof string !== "string") return "";
+    if (string.length === 0) return string;
+  
+    while (code.length < string.length) {
+      code += code;
+    }
+    code = code.substr(0, string.length);
+    let codeIndex = 0;
+  
+    return string
+      .split("")
+      .map((letter, index) => {
+        letter = letter.toLowerCase();
+        const aCode = "a".charCodeAt(0);
+        const letterNumber = letter.charCodeAt(0) - aCode;
+  
+        if (letterNumber < 0 || letterNumber > 25) return letter;
+  
+        const codeNumber = code.charCodeAt(codeIndex) - aCode;
+        codeIndex++;
+  
+        return String.fromCharCode(((letterNumber + codeNumber) % 26) + aCode);
+      })
+      .join("");
+  }
+
